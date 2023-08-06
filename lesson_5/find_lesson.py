0dfac769be484e26c3766c1cbe33d144e92a772a
Natalya Mastyaeva <nata.m.754@gmail.com>
2023-08-06 23:07:04 +0400

    add: find_lesson.py

---
+++ lesson_5/find_lesson.py
@@ -0,0 +1,12 @@
+import time
+
+from selenium import webdriver
+from webdriver_manager.chrome import ChromeDriverManager
+from selenium.webdriver.chrome.service import Service
+
+service = Service(executable_path=ChromeDriverManager().install())
+driver = webdriver.Chrome(service=service)
+
+driver.get('https://www.freeconferencecall.com/login')
+driver.find_element('id', 'loginformsubmit').click()
+time.sleep(3)
