a1e561f62413de3ed063bde5fbd91971c6d0f9ec
Natalya Mastyaeva <nata.m.754@gmail.com>
2023-08-07 00:30:50 +0400

    add: click_tutor.py

---
+++ lesson_8/click_tutor.py
@@ -0,0 +1,27 @@
+import time
+
+from selenium import webdriver
+from webdriver_manager.chrome import ChromeDriverManager
+from selenium.webdriver.chrome.service import Service
+
+service = Service(executable_path=ChromeDriverManager().install())
+driver = webdriver.Chrome(service=service)
+
+driver.get('https://www.freeconferencecall.com/global/gl')
+
+login_button = driver.find_element('xpath', '//a[@id="login-desktop"]')
+login_button.click()
+
+email_field = driver.find_element('xpath', '//input[@id="login_email"]')
+email_field.send_keys('test@test.ru')
+
+print(email_field.get_attribute('value'))
+print(email_field.get_attribute('maxlength'))
+
+email_field.send_keys('aaaaa')
+print(email_field.get_attribute('value'))
+
+email_field.clear()
+
+email_field.send_keys('aaaaa')
+print(email_field.get_attribute('value'))
