fbd95d2dd1f813b6368c465f14a40f8f82a909a9
Natalya Mastyaeva <nata.m.754@gmail.com>
2023-08-13 23:25:02 +0400

    ref: download.py

--- lesson_10/download.py
+++ lesson_10/download.py
@@ -12,7 +12,6 @@ chrome_options.add_experimental_option("prefs", prefs)
 service = Service(executable_path=ChromeDriverManager().install())
 driver = webdriver.Chrome(service=service, options=chrome_options)
 
-
 driver.get('https://the-internet.herokuapp.com/download')
 time.sleep(3)
 driver.find_elements("xpath", "//a")[3].click()
