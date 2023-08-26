03e6e0f57e78edf2c6285a1f2553100fb45fe150
Natalya Mastyaeva <nata.m.754@gmail.com>
2023-08-26 23:06:30 +0400

    ref: alerts.py

--- lesson_13/alerts.py
+++ lesson_13/alerts.py
@@ -8,16 +8,51 @@ from selenium.webdriver.support import expected_conditions as es
 
 service = Service(ChromeDriverManager().install())
 driver = webdriver.Chrome(service=service)
-wait = WebDriverWait(driver, 10, poll_frequency=1)
+wait = WebDriverWait(driver, 10, poll_frequency=0.3)
 
 driver.get("https://demoqa.com/alerts")
 
-BUTTON_1 = ("xpath", "//button[@id='alertButton']")
-wait.until(es.element_to_be_clickable(BUTTON_1)).click()
+# BUTTON_1 = ("xpath", "//button[@id='alertButton']")
+# wait.until(es.element_to_be_clickable(BUTTON_1)).click()
+#
+# alert = wait.until(es.alert_is_present())
+# time.sleep(3)
+# driver.switch_to.alert
+# time.sleep(3)
+# alert.accept()
+
+# BUTTON_3 = ("xpath", "//button[@id='confirmButton']")
+# wait.until(es.element_to_be_clickable(BUTTON_3)).click()
+#
+# alert = wait.until(es.alert_is_present())
+#
+# time.sleep(3)
+#
+# driver.switch_to.alert
+#
+# time.sleep(3)
+# print(alert.text)
+# alert.dismiss()
+#
+# time.sleep(3)
+
+BUTTON_4 = ("xpath", "//button[@id='promtButton']")
+wait.until(es.element_to_be_clickable(BUTTON_4)).click()
 
 alert = wait.until(es.alert_is_present())
+
 time.sleep(3)
+
 driver.switch_to.alert
+
 time.sleep(3)
+
+alert.send_keys('Wow!!!')
+
+time.sleep(3)
+
 alert.accept()
+
+time.sleep(3)
+
 driver.quit()
