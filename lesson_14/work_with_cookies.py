2d8165ba77a7f9918e551bc449c8128d17df0e3a
Natalya Mastyaeva <nata.m.754@gmail.com>
2023-08-28 01:16:27 +0400

    ref: work_with_cookies.py

--- lesson_14/work_with_cookies.py
+++ lesson_14/work_with_cookies.py
@@ -1,9 +1,11 @@
+import os
+import pickle
 import time
+
 from selenium import webdriver
 from webdriver_manager.chrome import ChromeDriverManager
 from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.support.ui import WebDriverWait
-from selenium.webdriver.support import expected_conditions as es
 
 
 service = Service(ChromeDriverManager().install())
@@ -11,9 +13,55 @@ driver = webdriver.Chrome(service=service)
 wait = WebDriverWait(driver, 10, poll_frequency=0.3)
 
 driver.get("https://www.freeconferencecall.com/en/us/login")
+
 # print(driver.get_cookie("country_code"))
 
-print(driver.get_cookies())
+# print(driver.get_cookies())
+
+# driver.add_cookie({
+#     "name": "Example",
+#     "value": "My_cooka"
+# })
+#
+# print(driver.get_cookie("Example"))
+
+# before = driver.get_cookie("split")
+# print(before)
+# driver.delete_cookie("split")
+# driver.add_cookie({
+#     "name": "split",
+#     "value": "SOMETHING"
+# })
+# after = driver.get_cookie("split")
+# print(after)
+
+# before = driver.get_cookies()
+# print(before)
+# driver.delete_all_cookies()
+# # driver.add_cookie({
+# #     "name": "split",
+# #     "value": "SOMETHING"
+# # })
+# after = driver.get_cookies()
+# print(after)
+
+# LOGIN_FIELD = ("id", "login_email")
+# PASSWORD_FIELD = ("id", "password")
+# SUBMIT_BUTTON = ("id", "loginformsubmit")
+# # Логинимся в аккаунт
+# driver.find_element(*LOGIN_FIELD).send_keys("autocheck@ya.ru")
+# driver.find_element(*PASSWORD_FIELD).send_keys("123")
+# driver.find_element(*SUBMIT_BUTTON).click()
+# pickle.dump(driver.get_cookies(), open(os.getcwd()+"/cookies/cookies.pkl", "wb"))
+
+driver.delete_all_cookies()
+cookies = pickle.load(open(os.getcwd()+"/cookies/cookies.pkl", "rb"))
+for cookie in cookies:
+    driver.add_cookie(cookie)
+
+time.sleep(5)
+driver.refresh()
+time.sleep(5)
 
 
 """
